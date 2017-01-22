8f3e11fee29ac90cb351fb624141c4678ee575ac
ArturN <nizamutdin.art@yandex.ru>
2017-01-22 17:10:23 +0400

    Added HtmlWebpackPlugin and CommonsChunkPlugin and other related changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
-const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const packages = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
 process.env.BABEL_ENV = TARGET;
+
 const PATHS = {
-  app: path.join(__dirname, 'app'),
+  appSrc: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build')
 };
 
 const common = {
+  context: PATHS.appSrc,
+
   entry: {
-    app: PATHS.app
+    app: './index.jsx',
+    vendor: Object.keys(packages.dependencies)
   },
 
   resolve: {
@@ -21,33 +26,44 @@ const common = {
 
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
 
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader',
-        include: PATHS.app
+        loaders: ['style-loader', 'css-loader'],
+        include: PATHS.appSrc
       },
 
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: PATHS.app
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: PATHS.appSrc
       },
 
       {
         test: /\.jsx?$/,
         loaders: [
-          'react-hot-loader',
-          'babel-loader?presets[]=es2015,presets[]=react,presets[]=stage-1&plugins[]=transform-decorators-legacy,plugins[]=transform-class-properties'
+          {
+            loader: 'react-hot-loader'
+          },
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015', 'react', 'stage-1'],
+              plugins: ['transform-decorators-legacy', 'transform-class-properties']
+            }
+          }
         ],
-        include: PATHS.app
+        include: PATHS.appSrc
       },
 
-      { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url-loader?limit=10000' }
+      {
+        test: /\.(woff2?|ttf|eot|svg)$/,
+        loader: 'url-loader?limit=10000'
+      }
     ]
   }
 };
@@ -55,7 +71,7 @@ const common = {
 //default configuration
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
-    devtool: 'cheap-eval-source-map',
+    devtool: 'cheap-module-source-map',
     devServer: {
       contentBase: PATHS.build,
       historyApiFallback: true,
@@ -66,10 +82,17 @@ if (TARGET === 'start' || !TARGET) {
       port: process.env.PORT || 3000
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Kanban app',
+        hash: true,
+        template: './index.html'
+      }),
       new webpack.HotModuleReplacementPlugin(),
-      new NpmInstallPlugin({
-        save: true // --save
-      })
+      new webpack.optimize.CommonsChunkPlugin({
+        name: ['common', 'vendor'],
+        minChunks: 2
+      })//,
+      //new webpack.optimize.UglifyJsPlugin() //todo: use in production mode
     ]
   });
 }
